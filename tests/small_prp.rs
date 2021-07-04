8a065ea2ca94db515b1830a78e1b40db2cef28dd
Dan Draper <dan@coderdan.co>
2021-07-04 21:14:48 +1000

    Don't leak the prng details outside of the PRP abstraction

--- tests/small_prp.rs
+++ tests/small_prp.rs
@@ -1,13 +1,10 @@
 use small_prp::Prp;
-use small_prp::prng::Prng;
 use hex_literal::hex;
 
 #[test]
 fn init_prp() {
     let key: [u8; 16] = hex!("00010203 04050607 08090a0b 0c0d0e0f");
-
-    let mut prg = Prng::init(&key);
-    let prp = Prp::init(&mut prg);
+    let prp = Prp::init(&key);
 
     // TODO: Test all numbers in the block
     println!("15 -> {}", prp.permute(15));
