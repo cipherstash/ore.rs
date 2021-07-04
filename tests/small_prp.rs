6c8f851c9f6ca234ad2c46c2e7797926ef0cfbb1
Dan Draper <dan@coderdan.co>
2021-07-04 20:45:54 +1000

    Refactoring

--- tests/small_prp.rs
+++ tests/small_prp.rs
@@ -1,20 +1,9 @@
-use aes::cipher::{
-    generic_array::arr,
-};
 use small_prp::Prp;
 use small_prp::prng::Prng;
 
 #[test]
-fn prg_next_byte() {
-    let key = arr![u8; 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
-    let mut prg = Prng::init(&key);
-    assert_eq!(219, prg.next_byte());
-    assert_eq!(69, prg.next_byte());
-}
-
-#[test]
 fn init_prp() {
-    let key = arr![u8; 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+    let key: Vec<u8> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
     let mut prg = Prng::init(&key);
     let prp = Prp::init(&mut prg);
 
