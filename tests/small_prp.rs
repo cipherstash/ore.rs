eba05d066b6f537271d1a59b4cd33363e8f76138
Dan Draper <dan@coderdan.co>
2021-07-04 21:11:05 +1000

    Added prng test and use hex for keys

--- tests/small_prp.rs
+++ tests/small_prp.rs
@@ -1,9 +1,11 @@
 use small_prp::Prp;
 use small_prp::prng::Prng;
+use hex_literal::hex;
 
 #[test]
 fn init_prp() {
-    let key: Vec<u8> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+    let key: [u8; 16] = hex!("00010203 04050607 08090a0b 0c0d0e0f");
+
     let mut prg = Prng::init(&key);
     let prp = Prp::init(&mut prg);
 
