9eb4bcaa9c044ff00ee16f4bfefb543f648bb1d6
Dan Draper <dan@coderdan.co>
2021-07-09 20:31:08 +1000

    WIP

--- benches/small_domain.rs
+++ benches/small_domain.rs
@@ -7,12 +7,18 @@ fn do_encrypt(ore: &mut Ore) {
   ore.encrypt(25);
 }
 
+#[inline]
+fn do_encrypt_left(ore: &mut Ore) {
+  ore.encrypt_left(25);
+}
+
 fn criterion_benchmark(c: &mut Criterion) {
     let prf_key: [u8; 16] = hex!("00010203 04050607 08090a0b 0c0d0e0f");
     let prp_key: [u8; 16] = hex!("d0d007a5 3f9a6848 83bc1f21 0f6595a3");
     let mut ore = Ore::init(prf_key, prp_key);
 
     c.bench_function("ore_small8", |b| b.iter(|| do_encrypt(black_box(&mut ore))));
+    c.bench_function("ore_small8_left", |b| b.iter(|| do_encrypt_left(black_box(&mut ore))));
 }
 
 criterion_group!(benches, criterion_benchmark);
