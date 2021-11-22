413432e2e0b0af3ba2ecaddb5acf1d99c49f6a8f
Dan Draper <dan@coderdan.co>
2021-11-22 21:13:03 +1100

    Added hash tests

--- src/hash.rs
+++ src/hash.rs
@@ -11,7 +11,8 @@ use aes::cipher::{
  * (uses AES in the Random Oracle model).
  * Returns only the LSB (useful for 1-bit indicator function).
  */
-pub fn hash(key: &[u8], input: &[u8]) -> u8 { // TODO: can we use a U3 type?
+// FIXME: The input should be exactly 16-bytes long (or technically the ORE block size)
+pub fn hash(key: &[u8], input: &[u8]) -> u8 {
     let key_array = GenericArray::from_slice(key);
     let cipher = Aes128::new(&key_array);
     // Can we clone into GenericArray directly? Are we doing an extra copy?
@@ -22,4 +23,24 @@ pub fn hash(key: &[u8], input: &[u8]) -> u8 { // TODO: can we use a U3 type?
     return output[0] & 1u8;
 }
 
-// TODO: Test internal modules internally?
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use hex_literal::hex;
+
+    #[test]
+    fn hash_test_1() {
+        let key: [u8; 16] = hex!("00010203 04050607 08090a0b 0c0d0e0f");
+        let input: [u8; 16] = hex!("00010203 04050607 08090a0b 0c0d0eaa");
+
+        assert_eq!(1u8, hash(&key, &input));
+    }
+
+    #[test]
+    fn hash_test_2() {
+        let key: [u8; 16] = hex!("00010203 04050607 08090a0b 0c0d0e0f");
+        let input: [u8; 16] = hex!("00010203 04050607 08090a0b 0c0d0e0f");
+
+        assert_eq!(0u8, hash(&key, &input));
+    }
+}
