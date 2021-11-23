4a04cb6c6d4883b1e353237f393a0305c51a2934
Dan Draper <dan@coderdan.co>
2021-11-23 15:22:57 +1100

    Additional tests for hash module

--- src/hash.rs
+++ src/hash.rs
@@ -11,8 +11,14 @@ use aes::cipher::{
  * (uses AES in the Random Oracle model).
  * Returns only the LSB (useful for 1-bit indicator function).
  */
-// FIXME: The input should be exactly 16-bytes long (or technically the ORE block size)
 pub fn hash(key: &[u8], input: &[u8]) -> u8 {
+    /*
+     * Slice size is not known at compile time so we assert here
+     * We could do this with compile checks but this would require an additional
+     * copy (and doesn't entirely avoid runtime checks anyway)
+     * See https://stackoverflow.com/questions/38168956/take-slice-of-certain-length-known-at-compile-time
+    */
+    assert_eq!(input.len(), 16);
     let key_array = GenericArray::from_slice(key);
     let cipher = Aes128::new(&key_array);
     // Can we clone into GenericArray directly? Are we doing an extra copy?
@@ -40,6 +46,24 @@ mod tests {
     fn hash_test_2() {
         let key: [u8; 16] = hex!("00010203 04050607 08090a0b 0c0d0e0f");
         let input: [u8; 16] = hex!("00010203 04050607 08090a0b 0c0d0e0f");
+
+        assert_eq!(0u8, hash(&key, &input));
+    }
+
+    #[test]
+    #[should_panic(expected = "assertion failed")]
+    fn hash_test_input_too_small() {
+        let key: [u8; 16] = hex!("00010203 04050607 08090a0b 0c0d0e0f");
+        let input: [u8; 8] = hex!("00010203 04050607");
+
+        assert_eq!(0u8, hash(&key, &input));
+    }
+
+    #[test]
+    #[should_panic(expected = "assertion failed")]
+    fn hash_test_input_too_large() {
+        let key: [u8; 16] = hex!("00010203 04050607 08090a0b 0c0d0e0f");
+        let input: [u8; 24] = hex!("00010203 04050607 ffffffff bbbbbbbb cccccccc abababab");
 
         assert_eq!(0u8, hash(&key, &input));
     }
