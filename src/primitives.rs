9a32df2c60f752f116b3b5637741480da4d853be
Dan Draper <dan@coderdan.co>
2021-11-24 20:58:19 +1100

    Moved hash to the primitives module

--- src/primitives.rs
+++ src/primitives.rs
@@ -1,5 +1,6 @@
 
 pub mod prf;
+pub mod hash;
 
 pub trait PRF {
     // TODO: Use a PRFKey trait as the argument here
@@ -8,3 +9,8 @@ pub trait PRF {
     fn encrypt_all(&self, data: &mut [u8]);
 }
 
+pub trait Hash {
+    fn new(key: &[u8]) -> Self;
+    fn hash(&self, data: &[u8]) -> u8;
+    fn hash_all(&self, input: &[u8], output: &mut [u8]);
+}
