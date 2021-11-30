14a57ecba04f73a2c85b9ae1678a8ebd946ea9c0
Dan Draper <dan@coderdan.co>
2021-11-30 21:19:03 +1100

    Collapsed ore module into main lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,33 @@
 
-mod ore;
 mod encrypt;
 mod ciphertext;
 mod primitives;
 pub mod scheme;
 
-pub use crate::ore::ORECipher;
+//pub use crate::ore::ORECipher;
 pub use crate::encrypt::OREEncrypt;
 pub use crate::ciphertext::*;
 
+use crate::primitives::SEED64;
+pub type PlainText<const N: usize> = [u8; N];
+
+#[derive(Debug, Clone)]
+pub struct OREError;
+
+pub trait ORECipher: Sized {
+    type LeftBlockType;
+    type RightBlockType;
+
+    fn init(k1: [u8; 16], k2: [u8; 16], seed: &SEED64) -> Result<Self, OREError>;
+
+    fn encrypt_left<const N: usize>(
+        &mut self, input: &PlainText<N>
+    ) -> Result<Left<Self::LeftBlockType, N>, OREError>;
+
+    fn encrypt<const N: usize>(
+        &mut self, input: &PlainText<N>
+    ) -> Result<CipherText<Self::LeftBlockType, Self::RightBlockType, N>, OREError>;
+}
 
 #[cfg(test)]
 #[macro_use]
