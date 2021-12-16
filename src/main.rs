387ea73aa4d0092be7f9fd843803e58807a96e16
Dan Draper <dan@coderdan.co>
2021-12-16 20:43:12 +1100

    Fixed bug in compare_raw_slices and added confirmation tests

--- src/main.rs
+++ src/main.rs
@@ -9,17 +9,19 @@ use hex_literal::hex;
 fn main() {
     let k1: [u8; 16] = hex!("00010203 04050607 08090a0b 0c0d0e0f");
     let k2: [u8; 16] = hex!("00010203 04050607 08090a0b 0c0d0e0f");
+    let k3: [u8; 16] = hex!("04050607 08090a0b 0c0d0e0f 11111111");
     let seed = hex!("00010203 04050607");
 
-    let mut ore: OREAES128 = ORECipher::init(k1, k2, &seed).unwrap();
+    let mut ore1: OREAES128 = ORECipher::init(k1, k2, &seed).unwrap();
+    let mut ore2: OREAES128 = ORECipher::init(k3, k2, &seed).unwrap();
 
-    let cta = 0u64.encrypt(&mut ore).unwrap();
-    let ctb = 0u64.encrypt(&mut ore).unwrap();
-    //println!("1 > 50 = {}", cta > ctb);
-
-    println!("RET = {:?}", OREAES128::compare_raw_slices(&cta.to_bytes(), &ctb.to_bytes()));
+    let cta = 0u32.encrypt(&mut ore1).unwrap();
+    let ctb = 0u32.encrypt(&mut ore2).unwrap();
 
 
-    //let bytes = cta.to_bytes();
-    //let _ct = CipherText::<OREAES128, 8>::from_bytes(&bytes).unwrap();
+    let bytes1 = cta.to_bytes();
+    let bytes2 = ctb.to_bytes();
+    println!("BY1 {:?}\n\n", bytes1);
+    println!("BY2 {:?}\n\n", bytes2);
+    println!("RET = {:?}", OREAES128::compare_raw_slices(&bytes1, &bytes2));
 }
