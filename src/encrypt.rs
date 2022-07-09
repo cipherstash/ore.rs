3d7a71ff5055fb2e5cb4ee13f24d8e7fbae49f9f
Dan Draper <dan@coderdan.co>
2022-07-09 18:29:09 +1000

    Removed mutable requirement for ORE Cipher

--- src/encrypt.rs
+++ src/encrypt.rs
@@ -7,8 +7,8 @@ pub trait OREEncrypt<T: ORECipher> {
     type LeftOutput;
     type FullOutput;
 
-    fn encrypt_left(&self, cipher: &mut T) -> Result<Self::LeftOutput, OREError>;
-    fn encrypt(&self, input: &mut T) -> Result<Self::FullOutput, OREError>;
+    fn encrypt_left(&self, cipher: &T) -> Result<Self::LeftOutput, OREError>;
+    fn encrypt(&self, input: &T) -> Result<Self::FullOutput, OREError>;
 }
 
 // FIXME: I don't like that the cipher is mutable - its private members are mutable
@@ -20,7 +20,7 @@ impl<T: ORECipher> OREEncrypt<T> for u64 {
     type LeftOutput = Left<T, 8>;
     type FullOutput = CipherText<T, 8>;
 
-    fn encrypt_left(&self, cipher: &mut T) -> Result<Self::LeftOutput, OREError>
+    fn encrypt_left(&self, cipher: &T) -> Result<Self::LeftOutput, OREError>
     where
         T::LeftBlockType: CipherTextBlock,
     {
@@ -28,7 +28,7 @@ impl<T: ORECipher> OREEncrypt<T> for u64 {
         cipher.encrypt_left(&bytes)
     }
 
-    fn encrypt(&self, cipher: &mut T) -> Result<Self::FullOutput, OREError>
+    fn encrypt(&self, cipher: &T) -> Result<Self::FullOutput, OREError>
     where
         T::LeftBlockType: CipherTextBlock,
         T::RightBlockType: CipherTextBlock,
@@ -42,12 +42,12 @@ impl<T: ORECipher> OREEncrypt<T> for u32 {
     type LeftOutput = Left<T, 4>;
     type FullOutput = CipherText<T, 4>;
 
-    fn encrypt_left(&self, cipher: &mut T) -> Result<Self::LeftOutput, OREError> {
+    fn encrypt_left(&self, cipher: &T) -> Result<Self::LeftOutput, OREError> {
         let bytes = self.to_be_bytes();
         cipher.encrypt_left(&bytes)
     }
 
-    fn encrypt(&self, cipher: &mut T) -> Result<Self::FullOutput, OREError> {
+    fn encrypt(&self, cipher: &T) -> Result<Self::FullOutput, OREError> {
         let bytes = self.to_be_bytes();
         cipher.encrypt(&bytes)
     }
@@ -57,12 +57,12 @@ impl<T: ORECipher> OREEncrypt<T> for f64 {
     type LeftOutput = Left<T, 8>;
     type FullOutput = CipherText<T, 8>;
 
-    fn encrypt_left(&self, cipher: &mut T) -> Result<Self::LeftOutput, OREError> {
+    fn encrypt_left(&self, cipher: &T) -> Result<Self::LeftOutput, OREError> {
         let plaintext: u64 = self.map_to();
         plaintext.encrypt_left(cipher)
     }
 
-    fn encrypt(&self, cipher: &mut T) -> Result<Self::FullOutput, OREError> {
+    fn encrypt(&self, cipher: &T) -> Result<Self::FullOutput, OREError> {
         let plaintext: u64 = self.map_to();
         plaintext.encrypt(cipher)
     }
@@ -72,11 +72,11 @@ impl<T: ORECipher, const N: usize> OREEncrypt<T> for PlainText<N> {
     type LeftOutput = Left<T, N>;
     type FullOutput = CipherText<T, N>;
 
-    fn encrypt_left(&self, cipher: &mut T) -> Result<Self::LeftOutput, OREError> {
+    fn encrypt_left(&self, cipher: &T) -> Result<Self::LeftOutput, OREError> {
         cipher.encrypt_left(self)
     }
 
-    fn encrypt(&self, cipher: &mut T) -> Result<Self::FullOutput, OREError> {
+    fn encrypt(&self, cipher: &T) -> Result<Self::FullOutput, OREError> {
         cipher.encrypt(self)
     }
 }
